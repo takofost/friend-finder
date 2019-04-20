0b6c22151140877919843629ed4ba4d32adf66c6
Kenny Tran <trank7295@gmail.com>
2019-04-20 17:06:08 -0400

    init

---
+++ app/data/friends.js
@@ -0,0 +1,117 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'SpongeBob',
+		photo: 'https://pixel.nymag.com/imgs/daily/vulture/2018/11/27/27-spongebob-squarepants.w700.h467.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Patrick',
+		photo: 'https://pixel.nymag.com/imgs/daily/selectall/2018/03/05/05-patrick.w700.h467.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Gary',
+		photo: 'https://logos-download.com/wp-content/uploads/2016/09/Gary_the_Snail_picture_logo.png',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Squidward',
+		photo: 'https://mtv.mtvnimages.com/uri/mgid:ao:image:mtv.com:664289?quality=0.8&format=jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Eugene',
+		photo: 'https://www.clipartmax.com/png/middle/119-1196034_image-spongebob-mr-krabs.png',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Larry',
+		photo: 'https://vignette.wikia.nocookie.net/spongebob/images/7/76/Larry_current.png/revision/latest/scale-to-width-down/350?cb=20181231134825',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Sandy',
+		photo: 'https://vignette.wikia.nocookie.net/spongebob/images/8/84/Sandy_stock_art.png/revision/latest?cb=20181202012358',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	}
+];
+
+module.exports = friendsArray;
